de2016f8c1b30ea40cc65d8ea11542fc6a23a072
jorgebush11 <70361438+jorgebush11@users.noreply.github.com>
2020-10-15 17:47:40 -0500

    hook test 1

--- src/index.js
+++ src/index.js
@@ -5,14 +5,9 @@ import "../public/styles.scss";
 
 const root = document.querySelector("#root");
 
-function logRandom() {
-	console.log(Math.random());
-}
-
 function Button() {
-	const [counter, setCounter] = useState(0);
-	//return <button onClick={logRandom}>{counter}</button>;
-	return <button onClick={(_) => setCounter(counter + 1)}>{counter}</button>;
+	const [counter, setCounter] = useState(5);
+	return <button onClick={() => setCounter(counter * 2)}>{counter}</button>;
 }
 
 ReactDom.render(<Button />, root);
